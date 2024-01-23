2664eba885ed15fb168bc8cc347f2068712c3293
fadedead <fadedeadd@gmail.com>
2024-01-23 23:02:24 +0530

    Added: footer image

--- src/footer.js
+++ src/footer.js
@@ -1,12 +1,16 @@
 const { getLinks } = require("./links");
+const PandaTwo = require("./assets/panda-two.jpg");
 
 function getFooter() {
   const footerContainer = document.createElement("div");
   footerContainer.classList.add("footer");
 
+  const footerInfoContainer = document.createElement("div");
+  footerInfoContainer.classList.add("footer-info");
+
   const footerTitle = document.createElement("h3");
   footerTitle.innerText = "Contact me";
-  footerContainer.appendChild(footerTitle);
+  footerInfoContainer.appendChild(footerTitle);
 
   const footerInfo = document.createElement("div");
 
@@ -23,11 +27,18 @@ function getFooter() {
   contact.innerHTML = "555-555-5555 <br> panda.is.cute@forest.com";
   footerInfo.appendChild(contact);
 
-  footerContainer.appendChild(footerInfo);
+  footerInfoContainer.appendChild(footerInfo);
 
   const iconsContainer = getLinks();
   iconsContainer.classList.add("about-dev-container");
-  footerContainer.appendChild(iconsContainer);
+  footerInfoContainer.appendChild(iconsContainer);
+
+  footerContainer.appendChild(footerInfoContainer);
+
+  const pandaTwo = new Image();
+  pandaTwo.classList.add("footer-image-container");
+  pandaTwo.src = PandaTwo;
+  footerContainer.appendChild(pandaTwo);
 
   return footerContainer;
 }
